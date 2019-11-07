002f0576482134261a37b321612af5cae5acac69
Lucas <lucasfiorini@msn.com>
2019-11-07 00:49:59 -0300

    :tada:

---
+++ src/knn_classifier.py
@@ -0,0 +1,85 @@
+import csv
+import math
+
+
+class knn_classifier:
+    classes = []
+    matrix = {}
+
+    def __init__(self, test):
+        self.classes = self.find_classes(test)
+        for classe in self.classes:
+            # TODO:
+            #  alocar n 0's para n classes, nao so 3 sempre
+            self.matrix[classe] = [0,0,0]
+
+    @staticmethod
+    def find_classes(test):
+        class_list = []
+        for list in test:
+            if list[4] not in class_list:
+                class_list.append(list[4])
+        #print(*class_list)
+        return class_list
+
+    @staticmethod
+    def read_file(file_name):
+        with open(file_name, 'r') as register:
+            #construtor de lista que cecebe um valor iteravel e cria uma lista
+            info = list(csv.reader(register))
+            # Retira informacao inutil de cabecalho
+            info.pop(0)
+            return info
+
+    def analyse(self, test_lists, training_lists, number_neighbours):
+        distances = []
+        map_data = {}
+        for list_test in test_lists:
+            distances.clear()
+            map_data.clear()
+            for list_training in training_lists:
+                distance = knn_classifier.dist(list_test, list_training)
+                distances.append(distance)
+                map_data[distance] = list_training
+                #print("{} {}".format("distancias: ", knn_classifier.dist(list_test, list_training)))
+            #ordenando por distancia para saber o mais proximo
+            distances.sort()
+
+            cluster_name = self.find_cluster(distances, map_data, number_neighbours)
+            self.update_matrix(cluster_name, list_test[4])
+
+    def find_cluster(self, distances, map_data, number_neighbours):
+        if number_neighbours == 1:
+            # Retorna nome da classe do mais proximo (menor distancia)
+            return map_data[distances[0]][4]
+        else:
+            neighbours = []
+            occurrences = []
+            list_neighbours = distances[0:int(number_neighbours)]
+            for dists in list_neighbours:
+                neighbours.append(map_data[dists])
+
+            for classes in self.classes:
+                num_of_occurrences = 0
+                for list in neighbours:
+                    if list[4] == classes:
+                        num_of_occurrences -=- 1
+                occurrences.append(num_of_occurrences)
+        return self.classes[occurrences.index(max(occurrences))]
+
+    def update_matrix(self, new_cluster, old_cluster):
+        (self.matrix[old_cluster])[self.classes.index(new_cluster)] += 1
+
+    @staticmethod
+    def dist(param1, param2):
+        distance = 0
+        for i in range(4):
+            distance += pow(float(param1[i]) - float(param2[i]),2)
+            #heheheheh funciona
+            i -=-1
+        distance = math.sqrt(distance)
+        return distance
+
+    def show_matrix(self):
+        for line in self.classes:
+            print(self.matrix[line])
---
+++ src/main.py
@@ -0,0 +1,21 @@
+import sys
+from knn_classifier import knn_classifier
+
+
+def main():
+    try:
+        number_of_neighbours = sys.argv[1]
+    except:
+        print("Usage: python3 main.py <num_neighbours>",file=sys.stderr)
+        sys.exit(1)
+    test = knn_classifier.read_file('iris_test.csv')
+    #print(*test, sep='\n')
+    training = knn_classifier.read_file('iris_treino.csv')
+    #print(*training,sep='\n')
+    k = knn_classifier(test)
+    k.analyse(test, training, number_of_neighbours)
+    k.show_matrix()
+
+
+if __name__ == '__main__':
+    main()
